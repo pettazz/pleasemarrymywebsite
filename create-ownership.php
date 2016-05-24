5c91c5ab7b53714f68f26589983acdf733a5a69f
Nick Pettazzoni <pettazz@gmail.com>
2016-05-24 17:47:12 -0400

    draft order warning

--- create-ownership.php
+++ create-ownership.php
@@ -17,6 +17,12 @@
       <form class="form-signin" action="create-ownership-handler.php" method="POST">
         <h2 class="form-signin-heading">Claim a boy</h2>
         
+        <div class="alert alert-warning alert-dismissible" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <span class="glyphicon glyphicon-sort-by-order" aria-hidden="true"></span>
+          <strong>Be cool.</strong> This machine doesn't track draft order, so respect your league's order if there is one.
+        </div>
+
         <label for="inputContestant" class="">Available Boys for week <?php echo $_GET['week']; ?></label>
         <select id="inputContestant" class="form-control input-lg" name="inputContestant">
           <option disabled selected value></option>
